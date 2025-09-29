fccaf21abed20acfd19aefc3acd297067d6c5d74
po3rdlp <leonpolandos53@gmail.com>
2025-09-29 15:13:04 +0800

    update service

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: "http://beckend-web-ygo-mdo-production.up.railway.app",
+    baseURL: "https://beckend-web-ygo-mdo-production.up.railway.app",
     timeout: 120000,
     headers: {"Content-Type" : "application/json"}
 })
